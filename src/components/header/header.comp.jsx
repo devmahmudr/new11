d81240c43327ffe36169ce4dcd30915f56252cd8
Maha <maha@gmail.com>
2025-05-12 19:21:47 +0500

    ready for backend

--- src/components/header/header.comp.jsx
+++ src/components/header/header.comp.jsx
@@ -2,15 +2,29 @@ import "./_header.comp.scss";
 
 export default function HeaderComp() {
   return (
-    <header className="header_comp">
-      <div className="header_comp_left">
-        <img src="/src/assets/lupa.svg" alt="search" className="seach_icon" />
-        <input type="search" name="search" id="search" placeholder="search" />
-      </div>
-      <div className="header_comp_right">
-        <img src="/src/assets/plus.svg" alt="plus" className="plus_icon" />
-        <button className="header_comp_add_btn">ADD NEW CUSTOMER</button>
-      </div>
-    </header>
+    <div className="header_container">
+      <header className="header_comp">
+        <div className="header_comp_left">
+          <img src="/src/assets/lupa.svg" alt="search" className="seach_icon" />
+          <input type="search" name="search" id="search" placeholder="search" />
+        </div>
+        <div className="header_comp_right">
+          <img src="/src/assets/plus.svg" alt="plus" className="plus_icon" />
+          <button className="header_comp_add_btn">ADD NEW CUSTOMER</button>
+        </div>
+      </header>
+      {/* <table>
+        <thead>
+          <tr>
+            <th scope="col" className="header_id">Id</th>
+            <th scope="col" className="header_name">Name</th>
+            <th scope="col" className="header_desc">Description</th>
+            <th scope="col" className="header_status">Status</th>
+            <th scope="col" className="header_rate">Rate</th>
+            <th scope="col" className="header_deposite">Deposite</th>
+          </tr>
+        </thead>
+      </table> */}
+    </div>
   );
 }
--- src/container/table/table.container.jsx
+++ src/container/table/table.container.jsx
@@ -1,8 +1,61 @@
-import HeaderComp from "../../components/header/header.comp"
-import "./_table.container.scss"
+import HeaderComp from "../../components/header/header.comp";
+import "./_table.container.scss";
 
-export default function TableContainer(){
-    return(
-        <HeaderComp/>
-    )
+export default function TableContainer() {
+  return (
+    <div className="table_container">
+      <HeaderComp />
+      <table>
+        <thead>
+          <tr>
+            <th scope="col" className="header_id">
+              Id
+            </th>
+            <th scope="col" className="header_name">
+              Name
+            </th>
+            <th scope="col" className="header_desc">
+              Description
+            </th>
+            <th scope="col" className="header_status">
+              Status
+            </th>
+            <th scope="col" className="header_rate">
+              Rate
+            </th>
+            <th scope="col" className="header_balance">
+              Balance
+            </th>
+            <th scope="col" className="header_deposite">
+              Deposite
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">1</td>
+            <td className="header_name">Ann Culhane</td>
+            <td className="header_desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam?
+            </td>
+            <td className="header_status">OPEN</td>
+            <td className="header_rate">$70.00</td>
+            <td className="header_balance">-$270.00</td>
+            <td className="header_deposite">$500.00</td>
+          </tr>
+            <tr>
+            <td scope="row">1</td>
+            <td className="header_name">Ann Culhane</td>
+            <td className="header_desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam?
+            </td>
+            <td className="header_status">OPEN</td>
+            <td className="header_rate">$70.00</td>
+            <td className="header_balance">-$270.00</td>
+            <td className="header_deposite">$500.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
